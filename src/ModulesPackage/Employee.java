00457a1a59b3c94654fd54c9909e4ef81feb500a
lsandathara <dmlakchani@gmail.com>
2019-07-27 22:33:50 +0530

    employee getters setters

--- src/ModulesPackage/Employee.java
+++ src/ModulesPackage/Employee.java
@@ -3,5 +3,18 @@ package ModulesPackage;
 public class Employee {
 	private String empId;
 	private String empname;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
 
+	
 }
