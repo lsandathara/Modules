6b0c766f65236eab6b1a2b26d8f52a563320f18c
lsandathara <dmlakchani@gmail.com>
2019-07-27 22:28:43 +0530

    student class latest commit

--- src/ModulesPackage/Student.java
+++ src/ModulesPackage/Student.java
@@ -3,5 +3,18 @@ package ModulesPackage;
 public class Student {
 	private String stdId;
 	private String Name;
+	public String getStdId() {
+		return stdId;
+	}
+	public void setStdId(String stdId) {
+		this.stdId = stdId;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
 
+	
 }
