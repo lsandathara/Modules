d2384a0c06a44584fbd918804479a5f8da76654b
lsandathara <dmlakchani@gmail.com>
2019-07-27 21:53:13 +0530

    student class

--- src/ModulesPackage/Student.java
+++ src/ModulesPackage/Student.java
@@ -1,5 +1,21 @@
 package ModulesPackage;
 
 public class Student {
+	private String Id;
+	private String name;
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
